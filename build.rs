1d7d849fcbd1c69ae3afbbbd42632f950fe1d6b5
mdrokz <mohammadmunshi@gmail.com>
2023-10-03 17:49:50 +0530

    feat: invoke build only for offline feature

--- build.rs
+++ build.rs
@@ -4,15 +4,17 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("path.rs");
-
-    let mut f = File::create(&dest_path).unwrap();
-    write!(
-        f,
-        "pub const MANIFEST_DIR: &str = \"{}\";",
-        manifest_dir
-    ).unwrap();
+    if cfg!(feature = "offline") {
+        let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    
+        let out_dir = env::var("OUT_DIR").unwrap();
+        let dest_path = Path::new(&out_dir).join("path.rs");
+    
+        let mut f = File::create(&dest_path).unwrap();
+        write!(
+            f,
+            "pub const MANIFEST_DIR: &str = \"{}\";",
+            manifest_dir
+        ).unwrap();
+    }
 }
