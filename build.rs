b2ac13f51b1f87332c7389f04ff37164dde7ece1
Samir Djelal <ssdjelal@gmail.com>
2023-10-11 00:06:19 +0100

    :bug: Fix: Windows path separator

--- build.rs
+++ build.rs
@@ -7,6 +7,9 @@ fn main() {
     if cfg!(feature = "offline") {
         let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     
+        #[cfg(target_os = "windows")]
+        let manifest_dir = manifest_dir.replace(r#"\"#,r#"\\"#);
+    
         let out_dir = env::var("OUT_DIR").unwrap();
         let dest_path = Path::new(&out_dir).join("path.rs");
     
